3dfbe0ff949a8e6a5134e8a65f42d6fe76c71de9
nsot5 <42383998+nsot5@users.noreply.github.com>
2018-11-07 08:41:07 -0800

    guess game

--- notes/Julisa Saavedra- Guess Game.py
+++ notes/Julisa Saavedra- Guess Game.py
@@ -1,14 +1,20 @@
 # Random numbers
 import random
-a = (random.randint(0, 10))
+number = random.randint(0, 10)
 random_guess = int(input("guess the number between 0 and 10."))
-while a !="random_guess:":
-    if random_guess < a:
+guesses_left = 5
+win = False
+while guesses_left > 0 and not win:
+    if random_guess < number:
         print("The number is lower.")
         random_guess = int(input("Guess again."))
-    elif random_guess > a:
+        guesses_left -= 1
+    elif random_guess > number:
         print("Your number was to high.")
         random_guess = int(input("Guess again."))
-    else:
+        guesses_left -= 1
+    elif random_guess == number:
         print("You got it!")
-        break
+
+
+
