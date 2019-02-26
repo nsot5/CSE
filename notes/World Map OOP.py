73ec0054d4db7596e2bd593bfd961ca96ca57401
julisa <nsot@fresnou.org>
2019-02-26 08:43:13 -0800

    world map oop

--- notes/World Map OOP.py
+++ notes/World Map OOP.py
@@ -7,6 +7,20 @@ class Room(object):
         self.description = description
 
 
+class Player(object):
+    def __init__(self, starting_location):
+        self.health = 100
+        self.current_location = starting_location
+        self.inventory = []
+        self.damage = 10
+
+    def move(self, new_location):
+        """This method moves a character to a new location
+
+        :param new_location: The variable containing a room object
+        """
+        self.current_location = new_location
+
 # Option 1
 # Add dependent rooms after
 R19A = Room("R19A")
@@ -18,3 +32,31 @@ R19A.north = parking_lot
 #  Put them in quotes
 R19A = Room("R19A", 'parking_lot')
 parking_lot = Room('The parking Lot', None, "R19A")
+
+player = Player(R19A)
+
+
+
+playing = True
+directions = ['north', 'south' 'east', 'west', 'up', 'down']
+
+# Controller
+while playing:
+    print(player.current_location.name)
+    print(player.current_location.description)
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command.lower()in directions:
+        try:
+            #  command = 'north'
+            room_object = getattr(player.current_location, command)
+
+            #  NEEDED FOR OPTION 2
+            room_var = globals()[room_object]
+            
+            player.move(room_object)
+        except KeyError:
+            print("I can't go that way.")
+    else:
+        print("Command Not Recognized")
