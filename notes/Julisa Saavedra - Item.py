ca2c490b0063949ebb462b426dcd5ba25dd6af24
julisa <nsot@fresnou.org>
2019-03-19 08:43:14 -0700

    item

--- notes/Julisa Saavedra - Item.py
+++ notes/Julisa Saavedra - Item.py
@@ -18,4 +18,36 @@ class Sword(Item):
 
     def one_handed_sword(self):
         self.ability_type = -1
-        
+        print ("You attack the opponent with a one handed sword.")
+
+    def zweihander_sword(self):
+        self.ability_type = -1
+        print("You attack the opponent with a zweihander sword.")
+
+    def svardstav_sword(self):
+        self.ability_type = -1
+        print("You attack your opponent with a svardstav sword.")
+
+    def jian_sword(self):
+        self.ability_type = -1
+        print("You attack your opponent with a jian sword.")
+
+    def longsword_sword(self):
+        self.ability_type = -1
+        print("You attack your opponent with a longsword.")
+
+    def broadsword_sword(self):
+        self.ability_type = -1
+        print("You attack your opponent with a broadsword.")
+
+    def long_knife_and_short_sword(self):
+        self.ability_type= -1
+        print("You attack your opponent with a Long knife and short sword.")
+
+    def claymore_sword(self):
+        self.ability_type = -1
+        print("You attack your opponent with a claymore sword.")
+
+    def great_sword(self):
+        self.ability_type = -1
+        print("You attack your opponent with a great sword.")
